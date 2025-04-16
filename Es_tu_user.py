5a6a8ae95c84ed0e2350437e85a8cf3cd5aaa292
João Silva <49078354+Master55556@users.noreply.github.com>
2025-04-16 17:59:08 +0100

    #4 Atualizado para a forma automática

--- Es_tu_user.py
+++ Es_tu_user.py
@@ -1,39 +1,37 @@
 import sqlite3
 import time
-import getpass
-
-start_time = time.time()
-
-username = input("Nome de utilizador: ")
-password_input = getpass.getpass("Password: ") 
+import random
 
 conn = sqlite3.connect("Dados.db")
 cursor = conn.cursor()
 
-cursor.execute("SELECT password FROM users WHERE name = ?", (username,))
-resultado = cursor.fetchone()
+cursor.execute("SELECT name, password FROM users")
+utilizadores = cursor.fetchall() 
 
-if resultado is None:
-    print("Utilizador não encontrado. Tenta outra vez.")
-    autenticado = False
-else:
-    
-    password_input = getpass.getpass("Password: ")
-    password_correta = resultado[0]
 
-    if password_input == password_correta:
-        print("Autenticado com sucesso.")
-        autenticado = True
+random.shuffle(utilizadores)
+
+
+inicio = time.time()
+
+comparacoes = 0
+
+for nome, senha_correta in utilizadores:
+    cursor.execute("SELECT password FROM users WHERE name = ?", (nome,))
+    resultado = cursor.fetchone() # fetchone buscar só a primeira linha do resultado retornando uma sequencia unica
+
+    if resultado:
+        password = resultado[0]
+        autenticado = (senha_correta == password)
+        comparacoes += 1
     else:
-        print("Password incorreta.")
-        autenticado = False
+        print(f"Utilizador '{nome}' não encontrado (deu erro).")
 
 
+fim = time.time()
+tempo_total = fim - inicio
 
-end_time = time.time()
-tempo_total = end_time - start_time
-
-print("Autenticação:", autenticado)
+print(f"\nComparações realizadas: {comparacoes}")
 print(f"Tempo total: {tempo_total:.4f} segundos")
 
 conn.close()
